25e1e46e653547bbc8fac3042422054b09102919
ndourmouhammad <ndourmouhammad15@gmail.com>
2024-04-02 14:05:52 +0000

    regalges de l'index

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Commune de Patte d’Oie </title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-   <div class="container">
-<h1>
-    Enrôlement des membres de la commune de Patte d’Oie 
-PHP - POO - MySQL
-</h1>
-   </div> 
+    <div class="container">
+        <h1>
+            Enrôlement des membres de la commune de Patte d’Oie
+            PHP - POO - MySQL
+        </h1>
+    </div>
 </body>
+
 </html>
