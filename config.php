c557b9c1c9334faff76807ddadd7d6fdebe8d65b
mamebassine <mamebassine06@gmail.com>
2024-04-02 15:43:27 +0000

    Instanciation de la classe membre

--- config.php
+++ config.php
@@ -1,12 +1,22 @@
 <?php
 
-define('DSN', 'mysql:host=localhost;dbname=apprenant_db;charset=utf8');
+define('DSN', 'mysql:host=localhost;dbname=commune;charset=utf8');
 define('USER', 'root');
 define('PASSWORD', '');
 
 try {
     $cnx = new PDO(DSN, USER, PASSWORD);
+    $matricule="";
+    $nom="";
+    $prenom="";
+    $trancheAge="";
+    $sexe="";
+    $situationMatrimoniale="";
+    $statut="";
     
+
+    $membre = new Membre($matricule, $nom, $prenom, $trancheAge, $sexe,$situationMatrimoniale, $statut, $cnx);
+    $results = $membre->read();
 } catch (PDOException $erreur) {
     die('Erreur de connexion : ' . $erreur->getMessage());
 }
