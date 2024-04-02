b137ef851519f3785656c80e26672f69f54babe3
mamebassine <mamebassine06@gmail.com>
2024-04-02 13:38:50 +0000

    Ajout fichier index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Commune de Patte d’Oie </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <div class="container">
+<h1>
+    Enrôlement des membres de la commune de Patte d’Oie 
+PHP - POO - MySQL
+</h1>
+   </div> 
+</body>
+</html>
